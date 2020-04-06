3ef018d10bb8f042dbe0c056c3fc6982d95c6b5d
Steven Grzybowski <grzy7316@gmail.com>
2020-04-06 16:46:18 -0400

    Add project files.

---
+++ HexchatSBClient/ResultsMonitor/FileMontor.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace HexchatSBClient.ResultsMonitor
+{
+    public class FileMontor
+    {
+        public List<FileInfo>  CheckSBresults(string path )
+        {
+           
+            DirectoryInfo SBPath = new DirectoryInfo(path);
+            List<FileInfo> FileList = SBPath.GetFiles("SearchBot_results_*.zip").ToList();
+            //var Files = .ToList();
+            
+            return FileList; 
+            
+        }
+
+
+        //public void UnzipFile(FileInfo File )
+        //{
+        //    var Entries = ZipArchive.Entries
+        //}
+
+    }
+}
---
+++ HexchatSBTESTS/FileTesting/FileTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using NUnit.Compatibility;
+using HexchatSBClient;
+
+
+
+namespace HexchatSBTESTS.FileTesting
+{
+    [TestFixture]
+    class TestFileFinder    
+    {
+        public HexchatSBClient.ResultsMonitor.FileMontor FM = new HexchatSBClient.ResultsMonitor.FileMontor();
+
+       [Test]
+        public void TestFileGetter()
+        {
+            FM.CheckSBresults("Z:\\IRC Downloads");
+            Assert.That(() => FM.CheckSBresults("Z:\\IRC Downloads"), Throws.Nothing);
+
+        } 
+
+    }
+}
