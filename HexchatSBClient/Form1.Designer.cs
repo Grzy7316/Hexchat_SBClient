2ab6fbb793f2b86fab635a7478478301ee216adf
Steven Grzybowski <grzy7316@gmail.com>
2020-04-06 19:39:05 -0400

    Added BG File Scanning

--- HexchatSBClient/Form1.Designer.cs
+++ HexchatSBClient/Form1.Designer.cs
@@ -28,36 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.FolderPathBox = new System.Windows.Forms.TextBox();
+            this.ScanStartButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FilterBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ResultsGrid = new System.Windows.Forms.DataGridView();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ResultsGrid)).BeginInit();
             this.SuspendLayout();
             // 
-            // textBox1
+            // FolderPathBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(41, 55);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 20);
-            this.textBox1.TabIndex = 0;
+            this.FolderPathBox.Location = new System.Drawing.Point(16, 29);
+            this.FolderPathBox.Name = "FolderPathBox";
+            this.FolderPathBox.Size = new System.Drawing.Size(245, 20);
+            this.FolderPathBox.TabIndex = 0;
             // 
-            // button1
+            // ScanStartButton
             // 
-            this.button1.Location = new System.Drawing.Point(182, 51);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.ScanStartButton.Location = new System.Drawing.Point(267, 27);
+            this.ScanStartButton.Name = "ScanStartButton";
+            this.ScanStartButton.Size = new System.Drawing.Size(75, 23);
+            this.ScanStartButton.TabIndex = 1;
+            this.ScanStartButton.Text = "Start Scanning";
+            this.ScanStartButton.UseVisualStyleBackColor = true;
+            this.ScanStartButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Path to IRC Downloads";
+            // 
+            // FilterBox
+            // 
+            this.FilterBox.Location = new System.Drawing.Point(16, 70);
+            this.FilterBox.Name = "FilterBox";
+            this.FilterBox.Size = new System.Drawing.Size(245, 20);
+            this.FilterBox.TabIndex = 3;
+            this.FilterBox.TextChanged += new System.EventHandler(this.FilterBox_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(16, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(104, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Filter Search Results";
+            // 
+            // ResultsGrid
+            // 
+            this.ResultsGrid.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.ResultsGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ResultsGrid.Location = new System.Drawing.Point(-2, 105);
+            this.ResultsGrid.Name = "ResultsGrid";
+            this.ResultsGrid.Size = new System.Drawing.Size(802, 435);
+            this.ResultsGrid.TabIndex = 5;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 542);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ResultsGrid);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FilterBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ScanStartButton);
+            this.Controls.Add(this.FolderPathBox);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.ResultsGrid)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -65,8 +112,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox FolderPathBox;
+        private System.Windows.Forms.Button ScanStartButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FilterBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView ResultsGrid;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- HexchatSBClient/Form1.cs
+++ HexchatSBClient/Form1.cs
@@ -6,12 +6,17 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows.Forms;
+using HexchatSBClient.ResultsManager;
 
 namespace HexchatSBClient
 {
     public partial class Form1 : Form
     {
+        public ResultsManager.FileMonitor FM = new FileMonitor();
+        public ResultsManager.ResultFiltering RF = new ResultFiltering();
+        public List<String> UnfilteredResults = new List<string>();
         public Form1()
         {
             InitializeComponent();
@@ -19,7 +24,45 @@ namespace HexchatSBClient
 
         private void button1_Click(object sender, EventArgs e)
         {
+            UpdateDataGrid(); 
+            var  myTimer = new System.Timers.Timer();
+            myTimer.Interval = 30000;
+            myTimer.Enabled = true;
+            myTimer.Elapsed += new ElapsedEventHandler(myEvent);
+        
+        }
+
+        private void FilterBox_TextChanged(object sender, EventArgs e)
+        {
+            var FilteredResults = RF.FilteredResults(UnfilteredResults, FilterBox.Text);
+            var FilteredTable = RF.ResultsTable(FilteredResults);
+            ResultsGrid.DataSource = FilteredTable;
+        }
+
+        public bool UpdateDataGrid()
+        {
+            var Zipfiles = FM.ZipFileFinder(FolderPathBox.Text, "zip");
+            FM.UnzipResultFiles(Zipfiles);
+            var textfiles = FM.ZipFileFinder(FolderPathBox.Text, "txt");
+            UnfilteredResults = FM.Importedtxt(textfiles);
+            var Results = RF.FilteredResults(UnfilteredResults, FilterBox.Text);
+            var InitialResults = RF.ResultsTable(Results);
+            ResultsGrid.DataSource = InitialResults;
+            return true; 
+        }
+
+        private void myEvent(object source, ElapsedEventArgs e)
+        {
+            var Zipfiles = FM.ZipFileFinder(FolderPathBox.Text, "zip");
+            FM.UnzipResultFiles(Zipfiles);
+            var textfiles = FM.ZipFileFinder(FolderPathBox.Text, "txt");
+            UnfilteredResults = FM.Importedtxt(textfiles);
+            var Results = RF.FilteredResults(UnfilteredResults, FilterBox.Text);
+            var InitialResults = RF.ResultsTable(Results);
+            
 
         }
+
+
     }
-}
+} 
