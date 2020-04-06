6f0c496b6fff4416d277febc66a1f26779680186
Steven Grzybowski <grzy7316@gmail.com>
2020-04-06 17:56:15 -0400

    Added Zip Extraction & txt Import

--- HexchatSBClient/ResultsMonitor/FileMontor.cs
+++ HexchatSBClient/ResultsMonitor/FileMontor.cs
@@ -10,22 +10,77 @@ namespace HexchatSBClient.ResultsMonitor
 {
     public class FileMontor
     {
-        public List<FileInfo>  CheckSBresults(string path )
+        public List<FileInfo> ZipFileFinder(string path, string type)
         {
-           
+
             DirectoryInfo SBPath = new DirectoryInfo(path);
-            List<FileInfo> FileList = SBPath.GetFiles("SearchBot_results_*.zip").ToList();
-            //var Files = .ToList();
-            
-            return FileList; 
-            
+            List<FileInfo> FileList = SBPath.GetFiles("SearchBot_results_*." + type).ToList();
+            //List<String> Files = new List<String>();
+            //foreach (FileInfo F in FileList)
+            //{
+            //    Files.Add(F.FullName);
+            //}
+            //return Files; 
+            return FileList;
+
         }
 
 
-        //public void UnzipFile(FileInfo File )
-        //{
-        //    var Entries = ZipArchive.Entries
-        //}
+        public void UnzipResultFiles(List<FileInfo> fileInfos)
+        {
+            foreach (FileInfo FI in fileInfos)
+            {
+                ZipArchive ZF = ZipFile.OpenRead(FI.FullName);
+                foreach (ZipArchiveEntry E in ZF.Entries)
+                {
+                    try
+                    {
+                        E.ExtractToFile(FI.DirectoryName + "\\" + E.Name, false);
+                    }
+                    catch(IOException ex)
+                    {
+                        if (ex.Message.Contains("' already exists"))
+                        {
+                            
+                        }
+                        else
+                        {
+                            throw;
+
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        public List<string> Importedtxt(List<FileInfo> FilesList)
+        {
+            List<string> AllFileContents = new List<String>();
+            foreach (FileInfo F in FilesList)
+            {
+                List<String> FileContent = new List<string>();
+                var Filecontent = File.ReadAllLines(F.FullName);
+                int i = 0; //Counter so we can skip first 4 lines of searchbot results 
+                foreach (var s in Filecontent)
+                {
+                    if (i >= 4)
+                    {
+                        FileContent.Add(s);
+                    }
+                    i++;
+
+                }
+                AllFileContents.AddRange(FileContent);
+
+            }
+            return AllFileContents;
+
+        }
+
+
+
 
     }
 }
--- HexchatSBTESTS/FileTesting/FileTests.cs
+++ HexchatSBTESTS/FileTesting/FileTests.cs
@@ -11,18 +11,66 @@ using HexchatSBClient;
 
 namespace HexchatSBTESTS.FileTesting
 {
+
     [TestFixture]
-    class TestFileFinder    
+    class TestZipFileTools
+    {
+
+        public HexchatSBClient.ResultsMonitor.FileMontor FM = new HexchatSBClient.ResultsMonitor.FileMontor();
+        [Test]
+        public void TestZipFinder_Ex()
+        {
+            //FM.CheckSBresults("Z:\\IRC Downloads");
+            Assert.That(() => FM.ZipFileFinder("Z:\\IRC Downloads", "zip"), Throws.Nothing);
+
+        }
+        [Test]
+        public void TestZipFinder_HasRecords()
+        {
+            Assert.That(() => FM.ZipFileFinder("Z:\\IRC Downloads", "zip").Count > 0);
+        }
+
+        [Test]
+        public void TestUnZip_ex()
+        {
+            var Files = FM.ZipFileFinder("Z:\\IRC Downloads", "zip");
+
+            Assert.That(() => FM.UnzipResultFiles(Files), Throws.Nothing);
+
+        }
+
+       
+
+    }
+
+    [TestFixture]
+    class TestTxtFiles
     {
         public HexchatSBClient.ResultsMonitor.FileMontor FM = new HexchatSBClient.ResultsMonitor.FileMontor();
-
-       [Test]
-        public void TestFileGetter()
+        [Test]
+        public void TestFindTextFiles_ex()
         {
-            FM.CheckSBresults("Z:\\IRC Downloads");
-            Assert.That(() => FM.CheckSBresults("Z:\\IRC Downloads"), Throws.Nothing);
+            Assert.That(() => FM.ZipFileFinder("Z:\\IRC Downloads", "txt"), Throws.Nothing);
+        }
 
-        } 
+        [Test]
+        public void TestFindTextFiles_HasRecords()
+        {
+            Assert.That(() => FM.ZipFileFinder("Z:\\IRC Downloads", "zip").Count > 0);
+        }
+        [Test]
+        public void Importedtxt_ex()
+        {
+            var Files = FM.ZipFileFinder("Z:\\IRC Downloads", "txt");
+            Assert.That(() => FM.Importedtxt(Files), Throws.Nothing);
+
+        }
+        [Test]
+        public void Importedtxt_content()
+        {
+            var Files = FM.ZipFileFinder("Z:\\IRC Downloads", "txt");
+            Assert.That(() => FM.Importedtxt(Files).Count > 0 );
+        }
 
     }
 }
